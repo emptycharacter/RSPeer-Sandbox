5b239cd1c535771bbfad94f65e307f1ebb619b04
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-30 11:15:59 -1000

    Update Tree enum

---
+++ src/Script/Data/Location.java
@@ -0,0 +1,9 @@
+package Script.Data;
+
+import org.rspeer.runetek.api.movement.position.Area;
+
+public enum Location {
+
+    private Area treeArea;
+    private Tree[] trees;
+}
--- src/Script/Data/Tree.java
+++ src/Script/Data/Tree.java
@@ -1,4 +1,57 @@
 package Script.Data;
 
-public class Tree {
+import org.rspeer.runetek.api.movement.position.Area;
+
+public enum Tree {
+
+    TREE(),
+    OAK(),
+    WILLOW(),
+    MAPLE(),
+    YEW(),
+    ;
+
+    private int Level;
+    private Area treeArea;
+    private String treeName;
+    private String logName;
+
+    Tree(int level, Area treeArea, String treeName, String logName) {
+        Level = level;
+        this.treeArea = treeArea;
+        this.treeName = treeName;
+        this.logName = logName;
+    }
+
+    public int getLevel() {
+        return Level;
+    }
+
+    public void setLevel(int level) {
+        Level = level;
+    }
+
+    public Area getTreeArea() {
+        return treeArea;
+    }
+
+    public void setTreeArea(Area treeArea) {
+        this.treeArea = treeArea;
+    }
+
+    public String getTreeName() {
+        return treeName;
+    }
+
+    public void setTreeName(String treeName) {
+        this.treeName = treeName;
+    }
+
+    public String getLogName() {
+        return logName;
+    }
+
+    public void setLogName(String logName) {
+        this.logName = logName;
+    }
 }
