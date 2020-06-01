d3e28168cec1fbdd7116d3d5eb1c687bb7ae7d30
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-31 15:40:35 -1000

    GUI updates

--- src/Script/UI/GUI.java
+++ src/Script/UI/GUI.java
@@ -2,9 +2,12 @@ package Script.UI;
 
 import Script.Data.Location;
 import Script.Data.Tree;
+import net.miginfocom.swing.MigLayout;
+import org.rspeer.runetek.api.Game;
 import org.rspeer.script.task.Task;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends Task {
 
@@ -19,18 +22,21 @@ public class GUI extends Task {
 
     public GUI(){
         frame = new JFrame("Sandbox v1.0");
+        frame.setLayout(new MigLayout());
+        frame.setPreferredSize(new Dimension(200, 200));
 
         treesComboBox = new JComboBox(Tree.values());
         locationsComboBox = new JComboBox(Location.values());
         startBtn = new JButton("Start");
 
-        frame.add(treesComboBox);
-        frame.add(locationsComboBox);
-        frame.add(startBtn);
+        frame.add(treesComboBox, "wrap, growx");
+        frame.add(locationsComboBox, "wrap, growx");
+        frame.add(startBtn, "growx");
 
         startBtn.addActionListener(x -> startBtnHandler());
 
         frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        frame.setLocationRelativeTo(Game.getCanvas());
         frame.pack();
 
         frame.setVisible(true);
