049aabed205828363e001d975913444c3b8d6f79
Joshua Farin <farin.k.joshua@gmail.com>
2020-05-31 14:59:02 -1000

    Tree area enums

--- src/Script/Data/Location.java
+++ src/Script/Data/Location.java
@@ -3,7 +3,22 @@ package Script.Data;
 import org.rspeer.runetek.api.movement.position.Area;
 
 public enum Location {
+    LUMBRIDGE_TREE_AREA(Area.rectangular(3180, 3255, 3172, 3247), Tree.TREE),
+    LUMBRIDGE_OAK_AREA(Area.rectangular(3199, 3246, 3205, 3238), Tree.OAK, Tree.TREE);
 
     private Area treeArea;
     private Tree[] trees;
+
+    Location(Area treeArea, Tree... trees) {
+        this.treeArea = treeArea;
+        this.trees = trees;
+    }
+
+    public Area getTreeArea() {
+        return treeArea;
+    }
+
+    public Tree[] getTrees() {
+        return trees;
+    }
 }
